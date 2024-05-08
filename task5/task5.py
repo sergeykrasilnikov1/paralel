81595963065ea9b7bd4f8bbb963c3d33e1c162d1
sergeykrasilnikov1 <100011841+sergeykrasilnikov1@users.noreply.github.com>
2024-05-08 12:25:04 +1000

    Update task5.py

--- task5/task5.py
+++ task5/task5.py
@@ -26,8 +26,8 @@ def process_video_multithread(input_path, output_path, multithread):
     frames = []
     cap = cv2.VideoCapture(input_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_width = 640
+    frame_height = 480
 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_path, fourcc, fps, (640,480))
